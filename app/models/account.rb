232e95fa8b462b468ea41e77fe440f35cef15fe5
Hylton <goregehylton@yahoo.com>
2023-07-08 16:30:57 -0500

    Setup Active Storage

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,7 @@ class Account < ApplicationRecord
     belongs_to :primary_province, class_name: 'Province'
     belongs_to :secondary_province, class_name: 'Province'
     has_many :orders
+    has_many_attached :images
 
     validates :email, :password, :password_confirmation presence: true
     validates :password, confirmation: true
