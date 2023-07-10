548f50d4f3960ce2533f4e2af9bbe240134481e0
Hylton <goregehylton@yahoo.com>
2023-07-10 18:27:06 -0500

    Added Cart Items quantity updating features

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,28 @@
 class CartController < ApplicationController
   def index
     @products = Product.find(@cart.map { |a| a['id'] })
+    puts @cart.inspect
+  end
+
+  def update
+    puts params
+    qty = params[:quantity_param].to_i
+    id = params[:id].to_i
+
+    # Check if the value is equal to zero. If so, remove the item from the cart.
+    session[:cart].delete_if { |a| a['id'] == id && qty == 0 }
+
+    # Otherwise, check if the value is different from what is already in the cart, and update accordingly.
+    existing_item = session[:cart].find { |a| a['id'] == id }
+    if existing_item
+      existing_item['qty'] = qty
+    else
+      new_item = { 'id' => id, 'qty' => qty }
+      session[:cart] << new_item
+    end
+
+    puts session[:cart]
+
+    redirect_to cart_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
 
   get '/cart', to: 'cart#index', as: 'cart'
+  put '/cart', to: 'cart#update', as: 'update_cart'
   get '/search', to: "search#index"
   devise_for :accounts
 end
