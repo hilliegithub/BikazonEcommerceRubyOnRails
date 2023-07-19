64b6c8793920f5d97657eab138111feb54f1d2d4
Hylton <goregehylton@yahoo.com>
2023-07-18 20:11:44 -0500

    Added New & Updated product cards to home page. Also Homepage Hero added.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../../../vendor/assets/javascripts .js
-//= link_tree ../../../vendor/assets/stylesheets .css
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,11 @@ class HomeController < ApplicationController
     @products = Product.all.shuffle.take(6)
 
     # Product is new if created date and updated date is within the last 3 days
-    @newproducts = Product.where('created_at >= ?', 3.days.ago.to_date).where("strftime('%Y-%m-%dT%H:%M:00', created_at) = strftime('%Y-%m-%dT%H:%M:00', updated_at)")
+    @newproducts = Product.where('created_at >= ?', 3.days.ago.to_date).where("strftime('%Y-%m-%dT%H:%M:00', created_at) = strftime('%Y-%m-%dT%H:%M:00', updated_at)").take(3)
 
 
     # Product is updated if updated date is within the last 3 days
-    @updatedproducts = Product.where('updated_at >= ?', 3.days.ago).where("strftime('%Y-%m-%dT%H:%M:00', created_at) != strftime('%Y-%m-%dT%H:%M:00', updated_at)")
+    @updatedproducts = Product.where('updated_at >= ?', 3.days.ago).where("strftime('%Y-%m-%dT%H:%M:00', created_at) != strftime('%Y-%m-%dT%H:%M:00', updated_at)").take(3)
                               #.shuffle.order('updated_at DESC')
   end
 end
