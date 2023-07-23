fbba4aafbd6a2413fbf529f6d821e734890a59a4
Hylton <goregehylton@yahoo.com>
2023-07-22 21:28:23 -0500

    Edited the Home Controller to get the most recently updated product.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
 
 
     # Product is updated if updated date is within the last 3 days
-    @updatedproducts = Product.where('updated_at >= ?', 3.days.ago).where("strftime('%Y-%m-%dT%H:%M:00', created_at) != strftime('%Y-%m-%dT%H:%M:00', updated_at)").take(3)
+    @updatedproducts = Product.where('updated_at >= ?', 3.days.ago).where("strftime('%Y-%m-%dT%H:%M:00', created_at) != strftime('%Y-%m-%dT%H:%M:00', updated_at)").order(updated_at: :desc).limit(3)
                               #.shuffle.order('updated_at DESC')
   end
 end
